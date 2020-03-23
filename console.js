1997b783c73878f02ee4c3de4e92b7286eb14b54
DELUUXE <info@deluuxe.nl>
2020-03-23 15:41:43 +0100

    [fix] remove annoying and outdated console clear script

--- console.js
+++ console.js
@@ -1,10 +1,6 @@
 consolefun();
-setInterval(function() {
-    consolefun();
-}, 1000);
 
 function consolefun() {
     var css = "font-family: 'Roboto', sans-serif;color:#00adff;font-size:2em;";
-    console.clear();
-    console.log("%cWow, you have found your way into the developer tools, congrats. 🎉", css);
+    console.log("%cWelcome to the developer tools! 🎉", css);
 }
